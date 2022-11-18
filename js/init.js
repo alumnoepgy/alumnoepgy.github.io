742d999d32dca87a3bed62a3e5698eb062a7003e
alumnoepgy <118540738+alumnoepgy@users.noreply.github.com>
2022-11-18 07:56:31 -0600

    Update init.js

--- js/init.js
+++ js/init.js
@@ -8,11 +8,11 @@
  * servidor. */
 // @ts-ignore
 firebase.initializeApp({
-    apiKey: "AIzaSyDzZxk913MUi3ESjLwNK14W0t1c6HgUOWE",
-    authDomain: "bdfalumnox.firebaseapp.com",
-    projectId: "bdfalumnox",
-    storageBucket: "bdfalumnox.appspot.com",
-    messagingSenderId: "1002536188546",
-    appId: "1:1002536188546:web:cfa9fb33aec119e7fc529b",
-    measurementId: "G-5HHSLWQ24J"
+   apiKey: "AIzaSyDLSjHDl-SXwET71XQDIM0xRUKDXgvO3jc",
+  authDomain: "alumnoepgy.firebaseapp.com",
+  projectId: "alumnoepgy",
+  storageBucket: "alumnoepgy.appspot.com",
+  messagingSenderId: "901459317168",
+  appId: "1:901459317168:web:e1866a80525a91904f8675",
+  measurementId: "G-9VCG52Z1CG"
 });
